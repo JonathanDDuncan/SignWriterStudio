309eb957992bf060c7a607a6631686b0605720b5
Jonathan Duncan <duncanjonathan@yahoo.ca>
2015-04-27 13:00:09 -0400

    Clean code

--- SignWriterStudio.Database.Dictionary/Connections.vb
+++ SignWriterStudio.Database.Dictionary/Connections.vb
@@ -4,202 +4,192 @@ Namespace DictionaryDataSetTableAdapters
     Partial Public Class DictionaryTableAdapter
         Public Property PublicConnection() As SQLiteConnection
             Get
-                Return (Me.Connection)
-
+                Return (Connection)
             End Get
             Set(ByVal value As SQLiteConnection)
-                Me.Connection = value
+                Connection = value
             End Set
         End Property
-        Public Sub AssignConnection(ByVal conn As SQLiteConnection, _
-            Optional ByVal trans As SQLiteTransaction = Nothing)
-            Me.Connection = conn
+
+        Public Sub AssignConnection(ByVal conn As SQLiteConnection,
+                                    Optional ByVal trans As SQLiteTransaction = Nothing)
+            Connection = conn
 
             If trans IsNot Nothing Then
 
-                Me.Adapter.InsertCommand.Transaction = trans
+                Adapter.InsertCommand.Transaction = trans
 
-                Me.Adapter.DeleteCommand.Transaction = trans
+                Adapter.DeleteCommand.Transaction = trans
 
-                Me.Adapter.UpdateCommand.Transaction = trans
+                Adapter.UpdateCommand.Transaction = trans
 
 
             End If
 
 
+            Adapter.AcceptChangesDuringUpdate = False
 
-            Me.Adapter.AcceptChangesDuringUpdate = False
-
-            Me.Adapter.ContinueUpdateOnError = False
-
+            Adapter.ContinueUpdateOnError = False
         End Sub
     End Class
+
     Partial Public Class DictionaryGlossTableAdapter
         Public Property PublicConnection() As SQLiteConnection
             Get
-                Return (Me.Connection)
-
+                Return (Connection)
             End Get
             Set(ByVal value As SQLiteConnection)
-                Me.Connection = value
+                Connection = value
             End Set
         End Property
-        Public Sub AssignConnection(ByVal conn As SQLiteConnection, _
-           Optional ByVal trans As SQLiteTransaction = Nothing)
-            Me.Connection = conn
+
+        Public Sub AssignConnection(ByVal conn As SQLiteConnection,
+                                    Optional ByVal trans As SQLiteTransaction = Nothing)
+            Connection = conn
 
             If trans IsNot Nothing Then
 
-                Me.Adapter.InsertCommand.Transaction = trans
+                Adapter.InsertCommand.Transaction = trans
 
-                Me.Adapter.DeleteCommand.Transaction = trans
+                Adapter.DeleteCommand.Transaction = trans
 
-                Me.Adapter.UpdateCommand.Transaction = trans
+                Adapter.UpdateCommand.Transaction = trans
 
 
             End If
 
 
+            Adapter.AcceptChangesDuringUpdate = False
 
-            Me.Adapter.AcceptChangesDuringUpdate = False
-
-            Me.Adapter.ContinueUpdateOnError = False
-
+            Adapter.ContinueUpdateOnError = False
         End Sub
     End Class
+
     Partial Public Class SignSymbolsTableAdapter
         Public Property PublicConnection() As SQLiteConnection
             Get
-                Return (Me.Connection)
-
+                Return (Connection)
             End Get
             Set(ByVal value As SQLiteConnection)
-                Me.Connection = value
+                Connection = value
             End Set
         End Property
-        Public Sub AssignConnection(ByVal conn As SQLiteConnection, _
-         Optional ByVal trans As SQLiteTransaction = Nothing)
-            Me.Connection = conn
+
+        Public Sub AssignConnection(ByVal conn As SQLiteConnection,
+                                    Optional ByVal trans As SQLiteTransaction = Nothing)
+            Connection = conn
 
             If trans IsNot Nothing Then
 
-                Me.Adapter.InsertCommand.Transaction = trans
+                Adapter.InsertCommand.Transaction = trans
 
-                Me.Adapter.DeleteCommand.Transaction = trans
+                Adapter.DeleteCommand.Transaction = trans
 
-                Me.Adapter.UpdateCommand.Transaction = trans
+                Adapter.UpdateCommand.Transaction = trans
 
 
             End If
 
 
+            Adapter.AcceptChangesDuringUpdate = False
 
-            Me.Adapter.AcceptChangesDuringUpdate = False
-
-            Me.Adapter.ContinueUpdateOnError = False
-
+            Adapter.ContinueUpdateOnError = False
         End Sub
     End Class
 
     Partial Public Class SignSequenceTableAdapter
         Public Property PublicConnection() As SQLiteConnection
             Get
-                Return (Me.Connection)
-
+                Return (Connection)
             End Get
             Set(ByVal value As SQLiteConnection)
-                Me.Connection = value
+                Connection = value
             End Set
         End Property
-        Public Sub AssignConnection(ByVal conn As SQLiteConnection, _
-         Optional ByVal trans As SQLiteTransaction = Nothing)
-            Me.Connection = conn
+
+        Public Sub AssignConnection(ByVal conn As SQLiteConnection,
+                                    Optional ByVal trans As SQLiteTransaction = Nothing)
+            Connection = conn
 
             If trans IsNot Nothing Then
 
-                Me.Adapter.InsertCommand.Transaction = trans
+                Adapter.InsertCommand.Transaction = trans
 
-                Me.Adapter.DeleteCommand.Transaction = trans
+                Adapter.DeleteCommand.Transaction = trans
 
-                Me.Adapter.UpdateCommand.Transaction = trans
+                Adapter.UpdateCommand.Transaction = trans
 
 
             End If
 
 
+            Adapter.AcceptChangesDuringUpdate = False
 
-            Me.Adapter.AcceptChangesDuringUpdate = False
-
-            Me.Adapter.ContinueUpdateOnError = False
-
+            Adapter.ContinueUpdateOnError = False
         End Sub
-
     End Class
+
     Partial Public Class PuddleTextTableAdapter
         Public Property PublicConnection() As SQLiteConnection
             Get
-                Return (Me.Connection)
-
+                Return (Connection)
             End Get
             Set(ByVal value As SQLiteConnection)
-                Me.Connection = value
+                Connection = value
             End Set
         End Property
-        Public Sub AssignConnection(ByVal conn As SQLiteConnection, _
-         Optional ByVal trans As SQLiteTransaction = Nothing)
-            Me.Connection = conn
+
+        Public Sub AssignConnection(ByVal conn As SQLiteConnection,
+                                    Optional ByVal trans As SQLiteTransaction = Nothing)
+            Connection = conn
 
             If trans IsNot Nothing Then
 
-                Me.Adapter.InsertCommand.Transaction = trans
+                Adapter.InsertCommand.Transaction = trans
 
-                Me.Adapter.DeleteCommand.Transaction = trans
+                Adapter.DeleteCommand.Transaction = trans
 
-                Me.Adapter.UpdateCommand.Transaction = trans
+                Adapter.UpdateCommand.Transaction = trans
 
 
             End If
 
 
+            Adapter.AcceptChangesDuringUpdate = False
 
-            Me.Adapter.AcceptChangesDuringUpdate = False
-
-            Me.Adapter.ContinueUpdateOnError = False
-
+            Adapter.ContinueUpdateOnError = False
         End Sub
-
     End Class
+
     Partial Public Class FrameTableAdapter
         Public Property PublicConnection() As SQLiteConnection
             Get
-                Return (Me.Connection)
-
+                Return (Connection)
             End Get
             Set(ByVal value As SQLiteConnection)
-                Me.Connection = value
+                Connection = value
             End Set
         End Property
-        Public Sub AssignConnection(ByVal conn As SQLiteConnection, _
-         Optional ByVal trans As SQLiteTransaction = Nothing)
-            Me.Connection = conn
+
+        Public Sub AssignConnection(ByVal conn As SQLiteConnection,
+                                    Optional ByVal trans As SQLiteTransaction = Nothing)
+            Connection = conn
 
             If trans IsNot Nothing Then
 
-                Me.Adapter.InsertCommand.Transaction = trans
+                Adapter.InsertCommand.Transaction = trans
 
-                Me.Adapter.DeleteCommand.Transaction = trans
+                Adapter.DeleteCommand.Transaction = trans
 
-                Me.Adapter.UpdateCommand.Transaction = trans
+                Adapter.UpdateCommand.Transaction = trans
 
 
             End If
 
 
+            Adapter.AcceptChangesDuringUpdate = False
 
-            Me.Adapter.AcceptChangesDuringUpdate = False
-
-            Me.Adapter.ContinueUpdateOnError = False
-
+            Adapter.ContinueUpdateOnError = False
         End Sub
     End Class
 End Namespace
