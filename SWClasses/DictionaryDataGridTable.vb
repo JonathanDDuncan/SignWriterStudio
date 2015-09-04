8b9d00592158caf749f420dab009acaf7091cebb
Jonathan Duncan <duncanjonathan@yahoo.ca>
2015-09-04 14:46:17 -0400

    Centering images in Report

--- SWClasses/DictionaryDataGridTable.vb
+++ SWClasses/DictionaryDataGridTable.vb
@@ -6,9 +6,18 @@ Public Class DictionaryDataGridTable
     Inherits DictionaryDataSet.SignsbyGlossesBilingualDataTable
     Sub New()
 
-        Dim dc1 = New DataColumn("Tags", GetType(List(Of String)))
-        Dim dc2 = New DataColumn("OriginalTags", GetType(List(Of String)))
-        Columns.Add(dc1)
-        Columns.Add(dc2)
+        AddColumn(New DataColumn("Tags", GetType(List(Of String))))
+        AddColumn(New DataColumn("OriginalTags", GetType(List(Of String))))
+        AddColumn(New DataColumn("NormalizedGloss", GetType(String)))
+        AddColumn(New DataColumn("SWritingWidth", GetType(Int32)))
+        AddColumn(New DataColumn("SWritingHeight", GetType(Int32)))
+        AddColumn(New DataColumn("IllustrationWidth", GetType(Int32)))
+        AddColumn(New DataColumn("IllustrationHeight", GetType(Int32)))
+        AddColumn(New DataColumn("SignWidth", GetType(Int32)))
+        AddColumn(New DataColumn("SignHeight", GetType(Int32)))
+    End Sub
+
+    Private Sub AddColumn(ByVal dataColumn As DataColumn)
+        Columns.Add(dataColumn)
     End Sub
 End Class
--- SignWriterStudio.Dictionary/ReportForm.vb
+++ SignWriterStudio.Dictionary/ReportForm.vb
@@ -1,5 +1,5 @@
 ﻿Imports SignWriterStudio.SWClasses
-
+Imports SignWriterStudio.General.All
 Public Class ReportForm
 
     Private Sub btnReport_Click(sender As Object, e As EventArgs) Handles btnReport.Click
@@ -13,19 +13,42 @@ Public Class ReportForm
     End Sub
 
     Private Shared Function NormalizeandSort(ByVal dt As DataTable) As DataTable
-        Dim dc As New DataColumn()
-        dc.ColumnName = "NormalizedGloss"
-        dc.DataType = Type.GetType("System.String")
-        dt.Columns.Add(dc)
-
+        AddImageSizes(dt)
         For Each row In dt.Rows
             row.Item("NormalizedGloss") = Normalization.Normalize(row.Item("Gloss1"))
         Next
 
+
         Dim dt1 = Sort(dt, "NormalizedGloss", "ASC")
         Return dt1
     End Function
-   
+
+    Private Shared Sub AddImageSizes(ByVal dt As DataTable)
+        For Each row In dt.Rows
+            Dim sWriting As Image = Nothing
+            If (Not IsDbNull(row.Item("SWriting"))) Then
+                sWriting = ByteArraytoImage(row.Item("SWriting"))
+            End If
+            row.Item("SWritingWidth") = If(sWriting IsNot Nothing, sWriting.Width, 0)
+            row.Item("SWritingHeight") = If(sWriting IsNot Nothing, sWriting.Height, 0)
+
+            Dim illustration As Image = Nothing
+            If (Not IsDbNull(row.Item("Photo"))) Then
+                illustration = ByteArraytoImage(row.Item("Photo"))
+            End If
+            row.Item("IllustrationWidth") = If(illustration IsNot Nothing, illustration.Width, 0)
+            row.Item("IllustrationHeight") = If(illustration IsNot Nothing, illustration.Height, 0)
+
+            Dim sign As Image = Nothing
+            If (Not IsDbNull(row.Item("Sign"))) Then
+                sign = ByteArraytoImage(row.Item("Sign"))
+            End If
+            row.Item("SignWidth") = If(sign IsNot Nothing, sign.Width, 0)
+            row.Item("SignHeight") = If(sign IsNot Nothing, sign.Height, 0)
+
+        Next
+    End Sub
+
     Public Shared Function Sort(dt As DataTable, colName As String, direction As String) As DataTable
         dt.DefaultView.Sort = colName & " " & direction
         dt = dt.DefaultView.ToTable()
