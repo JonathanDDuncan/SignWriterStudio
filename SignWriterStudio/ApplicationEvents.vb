272d6ba8aebedd9c9839ac3781a4539147fccae9
JonathanDuncan <duncanjonathan@yahoo.ca>
2017-04-13 10:02:59 -0600

    Better logging of program crash

--- SignWriterStudio/ApplicationEvents.vb
+++ SignWriterStudio/ApplicationEvents.vb
@@ -1,4 +1,5 @@
-﻿
+﻿Imports System.Text
+
 Namespace My
 
     ' The following events are available for MyApplication:
@@ -11,11 +12,43 @@ Namespace My
     Partial Friend Class MyApplication
 
         Private Sub MyApplication_UnhandledException(sender As Object, e As ApplicationServices.UnhandledExceptionEventArgs) Handles Me.UnhandledException
+            WriteDetailedException(sender, e.Exception)
             My.Application.Log.WriteException(e.Exception,
        TraceEventType.Critical,
        "Application shut down at " &
        My.Computer.Clock.GmtTime.ToString)
+
         End Sub
+
+
+        Private Sub WriteDetailedException(sender As Object, exception As Exception)
+            Dim sb As New StringBuilder()
+            sb.Append("sender ")
+            sb.Append(sender.ToString)
+            sb.Append(" ,sender type: ")
+            sb.Append(sender.GetType)
+            sb.Append(DetailedException(exception))
+
+            My.Application.Log.WriteEntry(sb.ToString())
+        End Sub
+
+        Private Function DetailedException(exception As Exception) As String
+            Dim sb As New StringBuilder()
+            sb.Append(" ,exception message: ")
+            sb.Append(exception.Message)
+            sb.Append(" ,exception HResult: ")
+            sb.Append(exception.HResult)
+            sb.Append(" ,exception Message: ")
+            sb.Append(exception.Message)
+
+            If exception.InnerException IsNot Nothing Then
+                sb.Append(" ,InnerException: ")
+                sb.Append(DetailedException(exception.InnerException))
+            End If
+
+            Return sb.ToString()
+        End Function
+
     End Class
 
 End Namespace
